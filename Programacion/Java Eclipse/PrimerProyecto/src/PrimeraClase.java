688c8d074d33748fb8a4e29e048e3962c2cbcc7e
JuMiLuGa <lubian.ganete.juliomiguel.ald@gmail.com>
2022-10-14 18:08:10 +0200

    18:08

--- Programacion/Java Eclipse/PrimerProyecto/src/PrimeraClase.java
+++ Programacion/Java Eclipse/PrimerProyecto/src/PrimeraClase.java
@@ -1,7 +1,14 @@
 
 public class PrimeraClase {
 	public static void main(String args[]) {
-		System.out.println("Hola Mundo");
 		
+		int x, y;
+		x=144;
+		y=999;
+		
+		System.out.println("La suma de "+ x +" mas "+ y+ " es igual a "+ (x+y));
+		System.out.println("La resta de "+ x +" menos "+ y+ " es igual a "+ (x-y));
+		System.out.println("La multiplicacion de "+ x +" por "+ y +" es igual a "+ (x*y));
+		System.out.println("La division de "+ x +" entre "+ y+ " es igual a "+ (x/y));
 		}
 }
---
+++ Programacion/Java Eclipse/PrimerProyecto/src/SegundaClase.java
@@ -0,0 +1,16 @@
+
+public class SegundaClase {
+	public static void main(String args[]) {
+		
+		String nombre, direccion;
+		int telefono;
+		
+		nombre="Julio Miguel Lubian Gañete";
+		direccion="Camiño da Rabisa";
+		telefono=665363016;
+		
+		System.out.println("Nombre completo: "+ nombre);
+		System.out.println("Direccion domicilio: "+ direccion);
+		System.out.println("Numero telefono: "+ telefono);
+	}
+}
