5d5395d236c2884927ac08394e009f8aba535d37
JuMiLuGa <lubian.ganete.juliomiguel.ald@gmail.com>
2022-10-20 19:40:18 +0200

    20/10/2022

--- Programacion/Java Eclipse/PrimerProyecto/src/Ejercicio5.java
+++ Programacion/Java Eclipse/PrimerProyecto/src/Ejercicio5.java
@@ -1,9 +1,20 @@
-
+import java.util.*;
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Introduzca el primer numero");
+		double num1 = entrada.nextDouble();
+		System.out.print("Introduzca el segundo numero");
+		double num2 = entrada.nextDouble();
+		System.out.print("Introduzca el tercer numero");
+		double num3 = entrada.nextDouble();
 		
-
+		double media = ((num1+num2+num3)/3);
+		
+		System.out.println("La media entre los numeros" + num1 + " " + num2 + " " + " y " + num3 + " es igual a: " + media);
+		
+		entrada.close();
 	}
 
 }
