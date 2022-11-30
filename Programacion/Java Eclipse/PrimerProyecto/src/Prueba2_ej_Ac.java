c331778f9937b5710c3c97d7e1b3dfbf8f4ee50d
JuMiLuGa <lubian.ganete.juliomiguel.ald@gmail.com>
2022-11-30 19:23:12 +0100

    30/11/2022

---
+++ Programacion/Java Eclipse/PrimerProyecto/src/Prueba2_ej_Ac.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Prueba2_ej_Ac {
+
+	public static void main(String[] args) {
+
+		// definir los valores de las letras
+		final String letras = "TRWAGMYFPDXBNJZSQVHLCKET";
+
+		Scanner ent = new Scanner(System.in);
+
+		String dninumeros = "";
+
+		// pedir por teclado el dni
+		System.out.print("Introduzca un numero de DNI seguido de su letra, sin espacios: ");
+		String dni = ent.next();
+
+		ent.close();
+
+		// coger solo numeros
+		for (int i = 0; i < dni.length() - 1; i++) {
+			dninumeros = dninumeros + dni.charAt(i);
+		}
+
+		// pasar la cadena a entero
+		int dniint = Integer.parseInt(dninumeros);
+
+		// calcular que letra corresponde
+		int numletra = dniint % 23;
+
+		char letradni = letras.charAt(numletra);
+
+		String dnicompleto = dninumeros + letradni;
+
+		// comparamos el dni con el dni correcto
+		if (dnicompleto.equals(dni)) {
+			System.out.println("El DNI introducido es correcto.");
+		} else {
+
+			System.out.println("El DNI introducido no es correcto, deberia ser: " + dnicompleto + ".");
+
+		}
+	}
+
+}
---
+++ Programacion/Java Eclipse/PrimerProyecto/src/Prueba2_ej_Cc.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Prueba2_ej_Cc {
+
+	public static void main(String[] args) {
+
+		Scanner ent = new Scanner(System.in);
+
+		// pedimos por teclado el numero de filas
+		System.out.print("Introduzca la cantidad de filas: ");
+		int numF = ent.nextInt();
+
+		// pedimos por teclado el numero de columnas
+		System.out.print("Introduzca la cantidad de columnas: ");
+		int numC = ent.nextInt();
+
+		// pedimos por teclado el caracter del contorno
+		System.out.print("Introduzca el caracter para el contorno: ");
+		String cc = ent.next();
+
+		// pedimos por teclado el caracter del relleno
+		System.out.print("Introduzca el caracter para el relleno: ");
+		String cr = ent.next();
+
+		ent.close();
+
+		String[][] matriz = new String[numF][numC];
+
+		// dar un caracter a los valores de los bordes y del relleno
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				// BORDES
+				if (j == 0 || j == matriz[i].length - 1) {
+					matriz[i][j] = cc;
+				} else if (i == 0 || i == matriz.length - 1) {
+					matriz[i][j] = cc;
+					// RELLENO
+				} else {
+					matriz[i][j] = cr;
+				}
+
+			}
+
+		}
+		// Mostrar la matriz
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				System.out.print(matriz[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
